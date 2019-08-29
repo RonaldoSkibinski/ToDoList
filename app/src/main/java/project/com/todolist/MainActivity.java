f262bf4ad874aa200ebef920b078d1ff593f5e6f
Ronaldo Skibinski <ronaldoskibinski@outlook.com>
2019-08-29 18:36:35 -0300

    Set variables and layout components; Criated a database and a table

--- app/src/main/java/project/com/todolist/MainActivity.java
+++ app/src/main/java/project/com/todolist/MainActivity.java
@@ -2,13 +2,37 @@ package project.com.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    //Atributes
+    private EditText todoText;
+    private Button addBtn;
+    private ListView todoList;
+    private SQLiteDatabase dB;
+    private String dBName = "appDB";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set layout components
+        todoText = findViewById(R.id.todoText);
+        addBtn = findViewById(R.id.addBtn);
+        todoList = findViewById(R.id.todoList);
+
+        //Create DataBase if not exists
+        dB = openOrCreateDatabase(dBName, MODE_PRIVATE, null);
+
+        //Create Table if not exists
+        dB.execSQL("CREATE TABLE IF NOT EXISTS todolist(id INTEGER PRIMARY KEY AUTOINCREMENT, tarefa VARCHAR)");
+
     }
 }
