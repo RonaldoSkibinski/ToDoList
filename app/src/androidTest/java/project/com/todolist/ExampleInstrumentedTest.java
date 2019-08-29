db66d00f2b500b565a55af54ff8d1874983f5efb
Ronaldo Skibinski <ronaldoskibinski@outlook.com>
2019-08-29 18:24:07 -0300

    Added text input, save button and ListView to layout

--- app/src/androidTest/java/project/com/todolist/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/project/com/todolist/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package project.com.todolist;
 
 import android.content.Context;
+
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
--- app/src/main/java/project/com/todolist/MainActivity.java
+++ app/src/main/java/project/com/todolist/MainActivity.java
@@ -1,6 +1,7 @@
 package project.com.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
