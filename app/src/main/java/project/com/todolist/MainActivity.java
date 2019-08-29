e278742391e1385ce61adcacfa2d6f3f6839a3e8
Ronaldo Skibinski <ronaldoskibinski@outlook.com>
2019-08-29 19:27:37 -0300

    Created a saveTodo function

--- app/src/main/java/project/com/todolist/MainActivity.java
+++ app/src/main/java/project/com/todolist/MainActivity.java
@@ -3,12 +3,14 @@ package project.com.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -40,13 +42,28 @@ public class MainActivity extends Activity {
             addBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    String text = todoText.getText().toString();
-                    dB.execSQL("INSERT INTO todolist (todo) values ('" + text + "')");
+                    saveTodo(todoText.getText().toString());
                 }
             });
 
         }catch(Exception e) {
             e.printStackTrace();
+        }
+
+    }
+
+    //Save
+    private void saveTodo(String todo) {
+
+        if(!todo.equals("")) {
+            try {
+                dB.execSQL("INSERT INTO todolist (todo) values ('" + todo + "')");
+                Toast toast = Toast.makeText(this, "Tarefa Salva", Toast.LENGTH_LONG);
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+        }else {
+            Toast toast = Toast.makeText(this, "Digite Uma Tarefa", Toast.LENGTH_LONG);
         }
 
     }
