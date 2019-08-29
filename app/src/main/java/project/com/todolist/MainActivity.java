93c563102e52d0b80ff34bef81ebf8c6f783f17d
Ronaldo Skibinski <ronaldoskibinski@outlook.com>
2019-08-29 20:07:08 -0300

    Upgraded SQL method to return the values and set into list

--- app/src/main/java/project/com/todolist/MainActivity.java
+++ app/src/main/java/project/com/todolist/MainActivity.java
@@ -1,16 +1,18 @@
 package project.com.todolist;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
@@ -19,6 +21,9 @@ public class MainActivity extends Activity {
     private Button addBtn;
     private ListView todoList;
     private SQLiteDatabase dB;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> itens;
+    private String getValues = "SELECT * FROM todolist ORDER BY id";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +35,14 @@ public class MainActivity extends Activity {
             //Set layout components
             todoText = findViewById(R.id.todoText);
             addBtn = findViewById(R.id.addBtn);
-            todoList = findViewById(R.id.todoList);
 
             createDB("appDb");
 
             //Create Table if not exists
-            sql("CREATE TABLE IF NOT EXISTS todolist(id INTEGER PRIMARY KEY AUTOINCREMENT, todo VARCHAR)");
+            sql("CREATE TABLE IF NOT EXISTS todolist(id INTEGER PRIMARY KEY AUTOINCREMENT, todo VARCHAR)", false);
+
+            //Get the table values and insert the values in the list
+            sql(getValues, true);
 
             addBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -53,12 +60,50 @@ public class MainActivity extends Activity {
     //Create db function
     private void createDB(String dBName) {
         //Create DataBase if not exists
-        dB = openOrCreateDatabase(dBName, MODE_PRIVATE, null);
+        try{
+            dB = openOrCreateDatabase(dBName, MODE_PRIVATE, null);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
     //Query function
-    private void sql(String sql) {
-        dB.execSQL(sql);
+    private void sql(String sql, boolean get) {
+        if(!get) {
+            try{
+                dB.execSQL(sql);
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+        }else {
+            //Create a cursor for get the values from table
+            Cursor cursor = dB.rawQuery(sql, null);
+
+            //toDOList
+            todoList = findViewById(R.id.todoList);
+
+            //Create index to cursor
+            int indiceColumnId = cursor.getColumnIndex("id");
+            int indiceColumnTodo = cursor.getColumnIndex("todo");
+
+            itens = new ArrayList<>();
+
+            //List the values
+            if(cursor.moveToFirst()) {
+                while(cursor.moveToNext()) {
+                    itens.add(cursor.getString(indiceColumnTodo));
+                }
+            }
+
+            //Instance the adapter
+            adapter = new ArrayAdapter<>(
+                    getApplicationContext(),
+                    android.R.layout.simple_list_item_1,
+                    android.R.id.text1,
+                    itens);
+
+            todoList.setAdapter(adapter);
+        }
     }
 
     //SaveTodo
@@ -67,7 +112,7 @@ public class MainActivity extends Activity {
         Toast toast = null;
         if(!todo.equals("")) {
             try {
-                sql("INSERT INTO todolist (todo) values ('" + todo + "')");
+                sql("INSERT INTO todolist (todo) values ('" + todo + "')", false);
                 toast = Toast.makeText(this, "Tarefa Salva", Toast.LENGTH_LONG);
             }catch(Exception e) {
                 e.printStackTrace();
@@ -76,6 +121,7 @@ public class MainActivity extends Activity {
             toast = Toast.makeText(this, "Digite Uma Tarefa", Toast.LENGTH_LONG);
         }
         toast.show();
+        sql(getValues, true);
 
     }
 
